eca65c36e57312569d9e6e61c835acf55e145f60
Douglas <dogge20@hotmail.com>
2022-09-13 14:58:00 +0200

    dijkstra

--- routy/map.erl
+++ routy/map.erl
@@ -1,5 +1,5 @@
 -module(map).
--export([new/0, update/3, reachable/2, all_reachable/3]).
+-export([new/0, update/3, reachable/2, all_reachable/3, all_nodes/1]).
 
 new() -> 
     [].
@@ -7,11 +7,20 @@ new() ->
 update(Node, Links, Map) -> 
     [{Node, Links}].
 
-all_reachable(Node, [], Reachable) -> {Node, Reachable};
+all_reachable(_, [], Reachable) -> Reachable;
 all_reachable(Node, [{Node, List}|T], Reachable) -> 
     all_reachable(Node, T, Reachable ++ List);
-all_reachable(Node, [H|T], Reachable) -> 
+all_reachable(Node, [_|T], Reachable) -> 
     all_reachable(Node, T, Reachable).
 
+
+all_nodes([], Nodes) -> Nodes;
+all_nodes([{Node, List}|T], Nodes) -> 
+	All = lists:append(List, Nodes),
+	all_nodes(T, lists:append([Node], All)).
 reachable(Node, Map) -> 
     all_reachable(Node, Map, []).
+
+all_nodes(Map) ->
+	all_nodes(Map, []).
+	
