240ff98a3512009e3f000efa247685549ec5e499
Douglas <dogge20@hotmail.com>
2022-09-13 14:58:28 +0200

    call correct reverse

--- routy/dijkstra.erl
+++ routy/dijkstra.erl
@@ -1,11 +1,10 @@
 -module(dijkstra).
 -export([update/4]).
 reverse(L) -> reverse(L,[]).
-
 reverse([],R) -> R;
 reverse([H|T],R) -> reverse(T,[H|R]). 
 update(_, _, _, [], Acc) ->
-	reverse(Acc,[]);
+	reverse(Acc);
 update(Node, N, Gateway, [{Node, _, _}|T], Acc) ->
 	update(Node, N, Gateway, T, Acc ++ [{Node, N, Gateway}]);
 update(Node, N, Gateway, [H|T], Acc) ->
